3315bbae4742f7033f8e52970a744170c024def8
Christian Zommerfelds <christian@bnotions.com>
2014-06-15 21:48:42 -0400

    multiple tests with mean and std deviation

--- src/main/scala/threesbrain/ai/TestAi.scala
+++ src/main/scala/threesbrain/ai/TestAi.scala
@@ -1,9 +1,22 @@
 package threesbrain.ai
 
+import scala.math
+import reflect.runtime.universe._
 import threesbrain.game.Play
 
 object TestAi extends App {
-    val end = Play.play(RandomPlayer)
-    println(end)
-    println("Score: " + end.score)
+    def mean(xs: List[Int]) = xs.sum.toDouble / xs.length
+    def stdDev(xs: List[Int]) = {
+        val m = mean(xs)
+        math.sqrt(xs.foldLeft(0.0)((x,y) => x + math.pow(m - y, 2)) / xs.length)
+    }
+
+    def className[T: TypeTag](x: T) = typeOf[T].typeSymbol.name
+
+    val nTests = 20
+    val playerToTest = RandomPlayer
+    val scores = List.fill(nTests)(Play.play(playerToTest).score)
+    println("Player: " + className(playerToTest))
+    println("Score mean: " + mean(scores))
+    println("Score std dev: " + stdDev(scores))
 }
