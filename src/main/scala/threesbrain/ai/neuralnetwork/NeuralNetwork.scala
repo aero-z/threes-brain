b8c0e52de8f0172d55d8078f159d77823ed20790
aero-z <aero_super@yahoo.com>
2014-06-18 22:47:26 -0400

    added basic feed forward neural network functionality

--- src/main/scala/threesbrain/ai/neuralnetwork/NeuralNetwork.scala
+++ src/main/scala/threesbrain/ai/neuralnetwork/NeuralNetwork.scala
@@ -1,5 +1,21 @@
 package threesbrain.ai.neuralnetwork
 
-class Neuron
+class Neuron(val weights: List[Double]) {
+    def evaluate(inputs: List[Double]) = {
+        assert(weights.length == inputs.length + 1)
+        (1.0 :: inputs).zip(weights).map({case (i, w) => i * w}).reduce(_ + _)
+    }
+}
 
-class NeuralNetwork(inputs: List[Neuron])
+class NeuronLayer(val neurons: List[Neuron]) {
+    def evaluate(inputs: List[Double]) = neurons.map(_.evaluate(inputs))
+}
+
+/**
+ * Layered feed forward network where every neuron is connected to all neurons in the previous layer
+ */
+class NeuralNetwork(val layers: List[NeuronLayer]) {
+    def evaluate(inputs: List[Double]) = {
+        layers.foldLeft(inputs)((outputs, layer) => layer.evaluate(outputs))
+    }
+}
--- src/main/scala/threesbrain/ai/neuralnetwork/NeuralNetworkPlayer.scala
+++ src/main/scala/threesbrain/ai/neuralnetwork/NeuralNetworkPlayer.scala
@@ -1,9 +1,21 @@
 package threesbrain.ai.neuralnetwork
 
 import threesbrain.game._
+import Move._
 
-object NeuralNetworkPlayer extends ThreesPlayer {
-    def decideMove(gameState: ThreesGame) = {
-        Move.Up
+class NeuralNetworkPlayer(neuralNetwork: NeuralNetwork) extends ThreesPlayer {
+    val numInputs = 13
+    val numOutputs = 1
+    assert(neuralNetwork.layers.head.neurons.length == numInputs)
+    assert(neuralNetwork.layers.last.neurons.length == numOutputs)
+    
+    def gameStateToNeuronInputs(gameState: ThreesGame): List[Double] =
+        ???
+    def neuronOutputsToMove(outputs: List[Double]): Move =
+        ???
+    def decideMove(gameState: ThreesGame): Move = {
+        val inputs = gameStateToNeuronInputs(gameState)
+        val outputs = neuralNetwork.evaluate(inputs)
+        neuronOutputsToMove(outputs)
     }
 }
